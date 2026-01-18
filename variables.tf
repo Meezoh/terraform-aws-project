281a54090b508b74c90a400527a474f71ca46b4e
Ubuntu <ubuntu@ip-172-31-35-191.us-east-2.compute.internal>
2026-01-18 08:31:08 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,11 +13,6 @@ variable "ssh_allowed_cidr" {
   type        = string
 }
 
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-  type        = string
-}
-
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
